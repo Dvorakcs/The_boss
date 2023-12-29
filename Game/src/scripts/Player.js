28a71a9abe39209569497d50107b39d0d9cc5848
Dvorakcs <mhsf15@hotmail.com>
2023-12-29 14:46:07 -0300

    Apply Gravity

--- Game/src/scripts/Player.js
+++ Game/src/scripts/Player.js
@@ -3,7 +3,10 @@ class Player{
         x:0,
         y:0
     };
-
+    #Velocity = {
+        x:0,
+        y:0
+    }
     #Width = 0;
     #Height = 0;
     #Sides = {
@@ -12,6 +15,7 @@ class Player{
         left:this.#Position.x,
         right:this.#Position.x + this.#Width
     }
+    #Gravity = 1
     constructor(config){
         this.#Position = config.position ?? {  x:0,y:0}
         this.#Width = config.width ?? 0
@@ -31,11 +35,12 @@ class Player{
     }
 
     UPDATE(EventUpdate){
-        
-        if(this.#Sides.bottom < (64 * 9)){
-            this.#Sides.bottom = this.#Position.y + this.#Height
-           
-            this.#Position.y += 2
+        this.#Position.y += this.#Velocity.y
+        if(this.#Sides.bottom + this.#Velocity.y < (64 * 9)){
+            this.#Velocity.y += this.#Gravity
+            this.#Sides.bottom = this.#Position.y + this.#Height + this.#Velocity.y
+        }else {
+            this.#Velocity.y = 0
         }
        
     }
